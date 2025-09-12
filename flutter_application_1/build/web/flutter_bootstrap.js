b90632b2a9247dd0070f6e49da40850ab9ee92a2
Your Name <you@example.com>
2025-09-12 23:03:54 +1000

    Update admin home page and remove approval dialog

--- flutter_application_1/build/web/flutter_bootstrap.js
+++ flutter_application_1/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1390137591"
+    serviceWorkerVersion: "3309538233"
   }
 });
--- flutter_application_1/build/web/flutter_service_worker.js
+++ flutter_application_1/build/web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4e291d39bc656f741037d45af7694872",
+"flutter_bootstrap.js": "835ac696e1d2ec5e303a19381b5a95c4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "acfffd1a083ede0eda1ab3d5f3f26ff8",
 "/": "acfffd1a083ede0eda1ab3d5f3f26ff8",
-"main.dart.js": "c3a0fc1fc871f87f856c603b7b300f0e",
+"main.dart.js": "fb0329052f889970e085a8b5cf824540",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7"};
 // The application shell files that are downloaded before a service worker can
